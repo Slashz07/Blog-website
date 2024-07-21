7e0e8a138e34337a36ff398003102e06d2b10395
Slashz07 <sumitforsaken76248@gmail.com>
2024-07-21 22:44:00 +0530

    authentication methods like register,login,checkLogin etc

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -9,7 +9,38 @@ class AuthService {
       .setProject(configEnvVar.appwriteProjectId)
     this.account=new Account(this.client)
   }
-  
+
+  async createAccount({emailId,password,name}) {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      const userAccount = await this.account.create(ID.unique(), emailId, password, name)
+      if (userAccount) {
+        return await this.loginUser({emailId,password})
+      } else {
+        return userAccount
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async loginUser({emailId,password}) {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      await this.account.createEmailPasswordSession(emailId,password)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async getCurrentUser(){
+    // eslint-disable-next-line no-useless-catch
+    try {
+      return await this.account.get()
+    } catch (error) {
+      throw error
+    }
+  }
 }
 
 const authService = new AuthService()
