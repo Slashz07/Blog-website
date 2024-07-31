0d2776b4ca3e9ec0534288782d2c4a0eea4af632
Slashz07 <sumitforsaken76248@gmail.com>
2024-07-31 16:08:50 +0530

    .

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import Header from "./header/Header.jsx"
 import Footer from "./footer/Footer.jsx"
+import Logo from "./Logo.jsx"
+import Container from "../container/Container.jsx"
+import LogoutBtn from "./header/LogoutBtn.jsx"
 
-export {Header,Footer}
+export {Container,Header,Footer,LogoutBtn,Logo}
