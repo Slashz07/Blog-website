6270b2c3da0eb6e9505e0f5c0c6e9e6b8ad081ee
Slashz07 <sumitforsaken76248@gmail.com>
2024-08-07 19:33:18 +0530

    .

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,9 @@ import Footer from "./footer/Footer.jsx"
 import Logo from "./Logo.jsx"
 import Container from "../container/Container.jsx"
 import LogoutBtn from "./header/LogoutBtn.jsx"
+import Input from './Input'
+import Button from './Button'
+import RTE from "./RTE.jsx"
+import Select from "./Select.jsx"
 
-export {Container,Header,Footer,LogoutBtn,Logo}
+export {Container,Header,Footer,LogoutBtn,Logo,Input,Button,RTE,Select}
