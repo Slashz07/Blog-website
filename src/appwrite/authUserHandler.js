417b61bb5e98618971b35e8dd04561e0fda2e80a
Slashz07 <sumitforsaken76248@gmail.com>
2024-09-24 06:44:13 +0530

    class created for accessing registered user's details via api key and scope management

---
+++ src/appwrite/authUserHandler.js
@@ -0,0 +1,37 @@
+import { Client, Account, ID,Users } from "node-appwrite";
+
+import configEnvVar from "../configure/config";
+import { Query } from "node-appwrite";
+
+class AuthUser {
+  client = new Client()
+  account
+  users;
+  constructor() {
+
+    this.client
+      .setEndpoint(configEnvVar.appwriteUrl)
+      .setProject(configEnvVar.appwriteProjectId)
+      .setKey(configEnvVar.appwriteAuthAPI)
+
+    this.users=new Users(this.client)
+
+  }
+
+  async getUser(userId) {
+    try {
+  
+      return await this.users.get(userId);
+    } catch (error) {
+      console.log("Could not fetch user with the provided id:", error);
+      return false;
+    }
+  }
+  
+}
+
+
+
+const authUser = new AuthUser()
+
+export default authUser
