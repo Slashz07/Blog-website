fd8fc02500d0f5056a089e208a7e77e78024a8a6
Slashz07 <sumitforsaken76248@gmail.com>
2024-09-27 18:48:20 +0530

    methods related to myAccount inncluding changing password,username ,saving account photo added

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -10,19 +10,27 @@ class AuthService {
     this.account=new Account(this.client)
   }
 
-  async createAccount({email,password,name}) {
+  async createAccount({email,password,name,userImage}) {
     // eslint-disable-next-line no-useless-catch
     try {
       const userAccount = await this.account.create(ID.unique(), email, password, name)
       console.log(userAccount)
       if (userAccount) {
-        return await this.loginUser({email,password})
+        const session=await this.loginUser({email,password})
+        await this.updateProfilePic(userImage)
+        return session
       } else {
         return userAccount
       }
     } catch (error) {
       throw error;
     }
+  }
+
+  async updateProfilePic(userImage){
+    return await this.account.updatePrefs({
+      userImage
+  });
   }
 
   async loginUser({email,password}) {
@@ -34,13 +42,45 @@ class AuthService {
     }
   }
 
+  getUserPreference = async (key) => {
+    try {
+      const preferences = await this.account.getPreferences();
+      const value = preferences[key];
+      return value;
+    } catch (error) {
+      console.error('Error fetching preferences:', error.message);
+    }
+  };
+  
+
+  changePassword = async (currentPassword, newPassword) => {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      await this.account.updatePassword(newPassword, currentPassword);
+      console.log('Password updated successfully');
+      // Optionally show a success message to the user
+    } catch (error) {
+      throw error
+    }
+  };
+  
+  updateUserName = async (newName) => {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      await this.account.updateName(newName);
+    } catch (error) {
+      throw error
+    }
+  };
+  
+
   async getCurrentUser(){
     // eslint-disable-next-line no-useless-catch
     try {
       return await this.account.get()
     } catch (error) {
-      throw error
-    }//return null
+      return null
+    }
   }
 
   async userLogout(){
